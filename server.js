a1770a0acbe5720e58e70c6c6515f2fc48a85b96
Alex <alexandrareginawilson@gmail.com>
2019-04-25 16:44:32 -0500

    Actually saving multipl enotes now

--- server.js
+++ server.js
@@ -78,7 +78,15 @@ app.get("/articles/:id", function(req, res) {
 app.post("/articles/:id", function(req, res) {
   db.Comment.create(req.body)
     .then(function(dbComment) {
-      return db.Article.findOneAndUpdate({_id: req.params.id}, { comments: dbComment._id }, { new: true });
+      return db.Article.findOneAndUpdate({
+        _id: req.params.id
+      }, { 
+        $push: { 
+          comments: dbComment._id 
+        }
+      }, { 
+          new: true 
+        });
     })
     .then(function(dbArticle) {
       res.json(dbArticle);
